949a71c027902ace706f5124a9e88de2dbf5d48d
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-11-04 23:21:27 +0200

    first

--- src/main/java/com/example/content/counter/ContentCounterApplication.java
+++ src/main/java/com/example/content/counter/ContentCounterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ContentCounterApplication {
 
 	public static void main(String[] args) {
+		System.err.println("hi");
 		SpringApplication.run(ContentCounterApplication.class, args);
 	}
 
