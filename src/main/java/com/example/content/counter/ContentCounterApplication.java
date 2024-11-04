84fa2d970318940edceae0d747e4f929c4c9bc30
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-11-04 23:19:50 +0200

    first commit

---
+++ src/main/java/com/example/content/counter/ContentCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.content.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCounterApplication.class, args);
+	}
+
+}
